278d6e3af2f385835f58d2112d72c07179cb00fd
iwantbasics <IWantBasics@cexs-MacBook-Air.local>
2024-08-11 03:11:09 +0100

    “update”

--- src/pages/api/events/index.ts
+++ src/pages/api/events/index.ts
@@ -1,17 +1,17 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiResponse } from 'next';
 import { authenticateJWT, AuthenticatedRequest } from '../../../lib/auth';
 import { pool } from '../../../lib/db';
 
 export default async function getAllEventsHandler(req: AuthenticatedRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
-    authenticateJWT(req, res, async () => {
-      try {
-        const result = await pool.query('SELECT events.*, users.fullname as created_by FROM events JOIN users ON events.user_id = users.id WHERE events.user_id = $1', [req.user.id]);
-        res.json(result.rows);
-      } catch (error) {
-        res.status(500).json({ message: 'Internal server error' });
-      }
-    });
+    try {
+      await authenticateJWT(req, res);
+      const result = await pool.query('SELECT events.*, users.fullname as created_by FROM events JOIN users ON events.user_id = users.id WHERE events.user_id = $1', [req.user.id]);
+      res.json(result.rows);
+    } catch (error) {
+      console.error('Error fetching events:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
   } else {
     res.setHeader('Allow', ['GET']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
